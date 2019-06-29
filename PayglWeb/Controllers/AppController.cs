6ac416f176ec1f95164a2e07748fc3f5b640ac78
Radek <radoslaw.taborski@gmail.com>
2019-06-30 01:54:51 +0200

    init - template

---
+++ PayglWeb/Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayglWeb.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Main";
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            return View();
+        }
+    }
+}
---
+++ PayglWeb/wwwroot/js/index.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("Hello js");
+
+    var button = $("#testButton");
+    button.on("click", function foo() {
+        alert("click");
+    });
+});
